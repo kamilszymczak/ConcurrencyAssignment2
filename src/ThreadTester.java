fcf3ef1624dc515c38b5788e1d4359778e538824
Sam <samfayhunt@gmail.com>
2020-03-04 14:00:40 +0000

    ThreadTester: new constructors for each sync class

--- src/ThreadTester.java
+++ src/ThreadTester.java
@@ -1,10 +1,20 @@
 public class ThreadTester implements Runnable {
     AtomicSync atomic;
+    SemaphoreSync semp;
+    ExtrinsicSync extS;
+    IntrinsicSync intS;
 
     public ThreadTester(AtomicSync a){
         this.atomic = a;
     }
 
+    public ThreadTester(SemaphoreSync s){this.semp = s;}
+
+    public ThreadTester(ExtrinsicSync e){this.extS = e;}
+
+    public ThreadTester(IntrinsicSync i){this.intS = i;}
+
+    
     public void run(){
 
         atomic.waitForThreads();
