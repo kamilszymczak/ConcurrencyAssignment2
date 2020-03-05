a5abbb369e1197374c73e5496b02055a882842d0
Kamil Szymczak <kamil_szymczak@hotmail.com>
2020-03-05 12:58:42 +0000

    ExtrinsicSyncTest: testPhase1a implemented not tested

--- src/ExtrinsicSyncTest.java
+++ src/ExtrinsicSyncTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
@@ -15,11 +18,27 @@ class ExtrinsicSyncTest {
 	
 	@Test
 	void testPhase1a() {
-		ExtrinsicSync sync = new ExtrinsicSync(Phase.ONE);
+		final int initThreads = 4;
+		ExtrinsicSync extrinsic = new ExtrinsicSync(Phase.ONE);
+
 		//Create some threads
-		//test method sync.waitForThreads()
-		
-		fail("Not yet implemented");
+		Thread threads[] = new Thread[initThreads];
+
+		for (int i = 0 ; i < initThreads; i++){
+			threads[i] = new Thread(new ThreadTester(extrinsic));
+			threads[i].start();
+		}
+
+		try{ Thread.sleep(initThreads);} catch (Exception e){System.out.println("Exception "+e.toString());}
+
+		Stack threadStack = new Stack<Thread>();
+
+		for (Thread startedThreads : threads){
+			if (startedThreads.getState() == Thread.State.TERMINATED) threadStack.push(startedThreads);
+		}
+
+		assertEquals (0, threadStack.size() % 4, "Number of terminated threads should be a multiple of 4");
+
 	}	
 	@Test
 	void testPhase1b() {
