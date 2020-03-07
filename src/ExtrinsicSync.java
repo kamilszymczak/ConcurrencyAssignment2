51ba79951bc3283c965fda0f1b1a7530146516b3
Kamil Szymczak <kamil_szymczak@hotmail.com>
2020-03-07 18:09:40 +0000

    ExtrinsicSync: new way of doing waitForThreads, works 90% of the time

--- src/ExtrinsicSync.java
+++ src/ExtrinsicSync.java
@@ -16,41 +16,89 @@ public class ExtrinsicSync implements Synchronisable {
 
 	Phase phase;
 	private final ReentrantLock[] locks = new ReentrantLock[4];
-	private final ReentrantLock loopLock = new ReentrantLock();
+	private final ReentrantLock releaseLock = new ReentrantLock();
+	private final ReentrantLock enterLock = new ReentrantLock();
 	private int lockedCounter = 0;
+	private int releasedCounter = 0;
+
 
 	// Constructor 
 	ExtrinsicSync (Phase p){ 
 		this.phase = p; // Phase of testing being performed
+		//Fix for Exception errors when locks are in array
+		for(int i = 0; i < locks.length; i++){
+			locks[i] = new ReentrantLock();
+		}
 	}
 
 	@Override
 	public void waitForThreads() {
+		//Note to self:- Lock can only be released by the thread that locked it
 
-		//check if all locks are locked if unlocked then lock one and skip for loop since only has to lock one
-		loopLock.lock();
-		for(ReentrantLock lock : locks) {
-			if (!lock.isLocked()) {
-				lock.lock();
-				loopLock.unlock();
+		//only let 4 threads to go to the loop others wait outside until all 4 threads leave/unlock
+		enterLock.lock();
+		try{
+			while(lockedCounter >= 4){ }
+			lockedCounter++;
+			System.out.println("Locked counter:- " + lockedCounter);
+		} finally {
+			enterLock.unlock();
+		}
+
+
+		//loop through locks if locked to one of them then after unlock break loop
+		for(int i = 0; i < locks.length; i++){
+			//if in if statement then lock acquired
+			if(locks[i].tryLock()){
 				try{
-					lockedCounter++;
-					while (lock.isLocked());
+					while(lockedCounter < 4);
 				} finally {
-					if(lockedCounter >= 4) {
-						loopLock.lock();
-						try {
-							lock.unlock();
+					locks[i].unlock();
+					//System.out.println("Thread unlocked:- " + Thread.currentThread().getName());
+					releaseLock.lock();
+					try{
+						releasedCounter++;
+						if(releasedCounter == 4){
+							System.out.println("Resetting counters");
+							releasedCounter = 0;
 							lockedCounter = 0;
-						} finally {
-							loopLock.unlock();
 						}
-
+					} finally {
+						releaseLock.unlock();
 					}
+					//if released 4 threads then reset counters
+					//System.out.println("Thread released incr:- " + releasedCounter);
 				}
-				continue;
+				break;
 			}
 		}
+
+
+		//STILL LEAVING THIS JUST INCASE
+		//check if all locks are locked if unlocked then lock one and skip for loop since only has to lock one
+//		loopLock.lock();
+//		for(ReentrantLock lock : locks) {
+//			if (!lock.isLocked()) {
+//				lock.lock();
+//				loopLock.unlock();
+//				try{
+//					lockedCounter++;
+//					while (lock.isLocked()) { System.out.println("LOCKED");};
+//				} finally {
+//					if(lockedCounter >= 4) {
+//						loopLock.lock();
+//						try {
+//							lock.unlock();
+//							lockedCounter = 0;
+//						} finally {
+//							loopLock.unlock();
+//						}
+//
+//					}
+//				}
+//				continue;
+//			}
+//		}
 
 		//thread is here if all 4 locks are locked (so actually no need to have lockedCounter?)
 		//if all locks are locked unlock them (this should be above for loop so that batch of 4 threads that are unlocked don't execute this if statement)
