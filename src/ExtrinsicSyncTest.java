6e89500b4788fc6f33f471376b891c9905a60d57
Kamil Szymczak <kamil_szymczak@hotmail.com>
2020-03-18 19:45:35 +0000

    ExstrinsicSyncTest: copied over test from atomic over to extrinsic

--- src/ExtrinsicSyncTest.java
+++ src/ExtrinsicSyncTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
 import java.util.Stack;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,6 +16,12 @@ import static org.junit.jupiter.api.Assertions.fail;
 class ExtrinsicSyncTest {
 	//Note it is expected that you may need multiple tests for each phase 
 	//Hence the structure below
+	private int largestMultipleFour(int x){
+		while (x % 4 != 0){
+			x--;
+		}
+		return x;
+	}
 	
 	@Test
 	void testPhase1a() {
@@ -51,10 +58,43 @@ class ExtrinsicSyncTest {
 	
 	@Test
 	void testPhase2a() {
+		Random rand = new Random();
+
+		int x = rand.nextInt(50);
+
+		// instantiate a random (even) number of threads
+		final int initThreads = (x%2 == 0 ? x : ++x);
+		System.out.println("Number of threads: "+initThreads);
+		//final int initThreads = 50;
 		ExtrinsicSync sync = new ExtrinsicSync(Phase.TWO);
-		//Create some threads
-		//test method sync.waitForThreadsInGroup
-		fail("Not yet implemented");
+		Thread threads[] = new Thread[initThreads];
+
+		// Even number required for this thread assignment strategy
+		for (int i = 0 ; i < initThreads/2; i++){
+			threads[i] = new Thread(new ThreadTester(sync, 0));
+			threads[i+(initThreads/2)] = new Thread((new ThreadTester(sync, 1)));
+			threads[i].start();
+			threads[i+(initThreads/2)].start();
+		}
+
+		try{ Thread.sleep(initThreads*2+500);} catch (Exception e){System.out.println("Exception "+e.toString());}
+
+		Stack threadStack = new Stack<Thread>();
+		Stack groupZeroStack = new Stack<Thread>();
+		Stack groupOneStack = new Stack<Thread>();
+
+		for (Thread startedThreads : threads){
+			if (startedThreads.getState() == Thread.State.TERMINATED) threadStack.push(startedThreads);
+		}
+
+		for (int i = 0 ; i < initThreads/2; i++){
+			if (threads[i].getState() == Thread.State.TERMINATED) groupZeroStack.push(threads[i]);
+			if (threads[i+(initThreads/2)].getState() == Thread.State.TERMINATED) groupOneStack.push(threads[i+(initThreads/2)]);
+		}
+
+		assertEquals(largestMultipleFour(initThreads/2), groupZeroStack.size(), "Wrong number of threads in group 0");
+		assertEquals(largestMultipleFour(initThreads/2), groupOneStack.size(), "Wrong number of threads in group 1");
+		assertEquals (0, threadStack.size() % 4, "Number of terminated threads should be a multiple of 4");
 	}
 	//etc
 	
