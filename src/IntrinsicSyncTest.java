76ef02f48e1bd44b10aafd3de4fce8009222aec2
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-04-22 14:28:00 +0100

    IntrinsicSyncTest: set waitTime in test

--- src/IntrinsicSyncTest.java
+++ src/IntrinsicSyncTest.java
@@ -442,7 +442,9 @@ public class IntrinsicSyncTest {
 		Stack terminatedStack = new Stack<Thread>();
 
 		for (int i = 0; i < initThreads; i++){
-			threads[i] = new Thread(new ThreadTester(intrinsic, 1));
+			ThreadTester test = new ThreadTester(intrinsic, 1);
+			test.setWaitTime(5000);
+			threads[i] = new Thread(test);
 			//System.out.println("Thread "+threads[i]+" assigned to group "+gid);
 			threads[i].start();
 		}
