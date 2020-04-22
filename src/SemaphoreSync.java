f77f95a256d4f3bdeae27575af08712da1755317
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-04-22 17:07:33 +0100

    SemaphoreSync FINALLY FIXED

--- src/SemaphoreSync.java
+++ src/SemaphoreSync.java
@@ -20,11 +20,19 @@ public class SemaphoreSync implements Synchronisable {
 
 	private class Group {
 
-		final Semaphore outerLock = new Semaphore(1, true);
-		final Semaphore syncIn = new Semaphore(4,true);
-		final Semaphore p3Lock = new Semaphore(4,true);
-		final Semaphore syncOut = new Semaphore(0,true);
+		final Semaphore outerLock = new Semaphore(4, true);
+		final Semaphore syncIn = new Semaphore(1,true);
+		final Semaphore syncOut = new Semaphore(1,true);
+		final Semaphore errorGuard = new Semaphore(1, true);
+		final Semaphore finalLock = new Semaphore(0, true);
+
+		final Semaphore counterIn = new Semaphore(4, true);
+		final Semaphore counterOut = new Semaphore(4, true);
 		final Semaphore numberOfCalls = new Semaphore(4,true);
+		final Semaphore p3Lock = new Semaphore(4,true);
+
+
+
 
 		private int groupID;
 
@@ -33,33 +41,44 @@ public class SemaphoreSync implements Synchronisable {
 
 		private void waitThreads() {
 			try {
-
-				// Only allow only 1 thread into the critical area
-				outerLock.acquire();
-
+				// four acquire A
+				errorGuard.acquire();
 				// phase 3 threads get locked here until 4 threads call finished
 				if (phase == Phase.THREE){
 					p3Lock.acquire();
 				}
-				// acquire 1 of 4 permits
+				errorGuard.release();
+				// 4th releases first 4 from next lock B
+				outerLock.acquire(); // 4 -> 0
+				//
 				syncIn.acquire();
-				while(syncIn.availablePermits() == 0 && syncOut.availablePermits() == 0){
-					// all syncIn permits acquired, release 4 permits from the next lock
-					syncOut.release(4);
+				counterIn.acquire(); // 4
+				while (counterIn.availablePermits() == 0 && finalLock.availablePermits() == 0){
+					finalLock.release(4);
 				}
-				// let more threads past the outerLock when less than 4 threads are inside
-				while(syncIn.availablePermits() != 0 && outerLock.availablePermits() == 0){
-					outerLock.release();
-				}
-				// threads get blocked here until 4 arrive
-				syncOut.acquire();
-
-				// release 4 permits (one each thread) to be aqcuired after the outerLock is released
 				syncIn.release();
 
-				// the last thread releases the outerLock permit
-				// allowing the first of the next 4 threads to enter the sync area
-				while (syncOut.availablePermits() == 0 && outerLock.availablePermits() == 0) outerLock.release();
+				finalLock.acquire();
+
+				syncOut.acquire();
+				counterOut.acquire();
+				while (counterOut.availablePermits() == 0 && syncOut.availablePermits() == 0 && outerLock.availablePermits() == 0){
+					if (finalLock.availablePermits() == 0){
+						// drain all threads for safety before resetting
+						finalLock.drainPermits();
+						syncIn.drainPermits();
+						counterIn.drainPermits();
+						outerLock.drainPermits();
+
+						counterOut.release(4);
+						counterIn.release(4);
+						syncIn.release(1);
+						outerLock.release(4);
+
+					}
+				}
+				syncOut.release();
+
 
 			} catch (Exception e) {
 				System.out.println("Semaphore exception " + e.toString());
